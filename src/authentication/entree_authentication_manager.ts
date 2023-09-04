bde9744d753016b36a776881db7312a3e72daa4e
Sven Jensen <sven.jensen@cbdevelopment.nl>
2023-09-04 21:57:45 +0200

    refactor: Manier van tokens opslaan verplaatst naar client

--- src/authentication/entree_authentication_manager.ts
+++ src/authentication/entree_authentication_manager.ts
@@ -11,16 +11,6 @@ class EntreeAuthenticationManager {
     private client?: BaseClient;
 
     /**
-     * The access token used to authenticate with Entree.
-     */
-    private accessToken?: string;
-
-    /**
-     * The refresh token used to refresh the access token.
-     */
-    private refreshToken?: string;
-
-    /**
      * Initializes the client used to communicate with Entree.
      */
     public async initializeClient() {
@@ -63,49 +53,15 @@ class EntreeAuthenticationManager {
         const params = this.getClient().callbackParams(req);
         const tokenSet = await this.getClient().callback(process.env.KN_REDIRECT_URI, params);
 
-        this.saveTokens(tokenSet);
-
         return tokenSet;
-    }
-
-    /**
-     * Saves the access and refresh tokens used to authenticate with Entree.
-     * @param tokenSet The access and refresh tokens used to authenticate with Entree.
-     */
-    public saveTokens(tokenSet: any) {
-        this.accessToken = tokenSet.access_token;
-        this.refreshToken = tokenSet.refresh_token;
-    }
-
-    /**
-     * Gets the user info from Entree.
-     * @returns The user info from Entree.
-     * @throws An error if the access token is not found.
-     * @throws An error if the refresh token is not found.
-     */
-    public async getUserInfo(): Promise<any> {
-        if (!this.accessToken || !this.refreshToken) {
-            throw new Error("Access token not found");
-        }
-
-        const userInfo = await this.getClient().userinfo(this.accessToken!);
-
-        return userInfo;
     }
 
     /**
      * Refreshes the access token.
      * @returns The new access and refresh tokens.
-     * @throws An error if the refresh token is not found.
      */
-    public async refreshTokens(): Promise<any> {
-        if (!this.refreshToken) {
-            throw new Error("Refresh token not found");
-        }
-
-        const tokenSet = await this.getClient().refresh(this.refreshToken!);
-
-        this.saveTokens(tokenSet);
+    public async refreshTokens(refreshToken: string): Promise<any> {
+        const tokenSet = await this.getClient().refresh(refreshToken);
 
         return tokenSet;
     }
