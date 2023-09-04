01eeddf437edb54bac30accc064434ec4ff74536
Sven Jensen <sven.jensen@cbdevelopment.nl>
2023-09-04 20:33:27 +0200

    feat: Basis routing

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,41 @@ app.use(session({
     }
 }));
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", async (req: Request, res: Response) => {
+    return res.render("index");
+});
+
+app.get("/agenda", async (req: Request, res: Response) => {
+    return res.render("agenda");
+});
+
+app.get("/messages", async (req: Request, res: Response) => {
+    return res.render("messages/index");
+});
+
+app.get("/messages/create", async (req: Request, res: Response) => {
+    return res.render("messages/create");
+});
+
+app.get("/absence", async (req: Request, res: Response) => {
+    return res.render("absence/index");
+});
+
+app.get("/absence/create", async (req: Request, res: Response) => {
+    return res.render("absence/create");
+});
+
+app.get("/results", async (req: Request, res: Response) => {
+    return res.render("results");
+});
+
+app.get("/profile", async (req: Request, res: Response) => {
+    return res.render("profile");
+});
+
 app.listen(3000, async () => {
     console.log("[INFO] Server gestart op http://localhost:3000");
 });
