b5242676e4cf61c2a7ac0a45ea5c7db5b2655e62
Sven Jensen <sven.jensen@cbdevelopment.nl>
2023-09-13 09:10:31 +0200

    fix: Entree user toegevoegd aan de paginas

--- src/routes/student_route.ts
+++ src/routes/student_route.ts
@@ -54,13 +54,17 @@ router.get("/absence", async (req: Request, res: Response) => {
 router.get("/absence/create", async (req: Request, res: Response) => {
     if(!req.session.authenticated) return res.redirect("/oauth2");
 
-    return res.render("absence/create");
+    return res.render("absence/create", {
+        entree_user: req.session.entree_user,
+    });
 });
 
 router.get("/results", async (req: Request, res: Response) => {
     if(!req.session.authenticated) return res.redirect("/oauth2");
 
-    return res.render("results");
+    return res.render("results", {
+        entree_user: req.session.entree_user,
+    });
 });
 
 router.get("/profile", async (req: Request, res: Response) => {
